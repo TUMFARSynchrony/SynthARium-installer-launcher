bd9c989f93937398cd1cbd74532c45852329c227
Hasan Hüseyin Kaçmaz <huseyinkacmaz14@gmail.com>
2024-08-08 15:54:27 +0200

    feat: update versions

--- src/renderer/components/Footer.tsx
+++ src/renderer/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className="mt-2 mb-1 grid grid-cols-3 gap-10 px-4 static bottom-0">
       <div className="font-montserrat rounded-lg border p-4 text-center shadow">
-        Version 0.0.10
+        Version 0.0.12
       </div>
       <div className="font-montserrat rounded-lg border p-4 text-center shadow">
         TUMFarSynchrony
